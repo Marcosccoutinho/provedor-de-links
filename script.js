aca185c7a61f7616beed0dc933e9b800ba2471c4
marcosccoutinho <marcao_hhc41@hotmail.com>
2023-01-20 17:42:12 -0300

    script update

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function togglemode() {
     const html = document.documentElement
     html.classList.toggle("light")
-   
+   // pegar uma tag img
+  const image = document.querySelector ( "#profile img" );
+
+  // substitui a imagem
+  if  ( html.classList.contains( "light" ))  {
+    // se tiver modo light, adicione a imagem light
+    image.setAttribute( "src" ,  "./imagens/marcos-oculos-light.png" );
+  }  else {
+    // define o modo de viver sem luz, mantém a imagem normal
+    image.setAttribute ( "src" ,  "./imagens/marcos-foto.png" );
+  } 
     
 }
